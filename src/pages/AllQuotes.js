86b296b7596bfa96538931f8c2248332811231a9
Djordje Knezevic <starwarrior@mycity.rs>
2021-10-18 17:13:44 +0200

    Add dummy quote data and input form for new quote

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Voja', text: 'The Bear asked the Rabbit why is his hair falling off' },
+    { id: 'q2', author: 'Princess Dileya', text: 'Like the Sun you shine, place of birth of ours' }
+];
+
 const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>
+    return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
 export default AllQuotes;
